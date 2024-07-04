2d12fda6d53eb74da1e1011822352657fd397623
Swapnil Mittal <72509297+swapnilmittal1@users.noreply.github.com>
2024-07-04 12:08:03 -0500

    Time: 0 ms (100.00%) | Memory: 41.3 MB (30.89%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int curr = 0;
+        int max = 0;
+
+        for(int i = 0; i < gain.length;i++){
+            curr = curr + gain[i];
+            max = Math.max(max, curr);
+        }
+        return max;
+    }
+}
