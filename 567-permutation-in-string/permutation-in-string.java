f6bb8ba210c533c2ee2e4d54377221ae5c88a588
Swapnil Mittal <72509297+swapnilmittal1@users.noreply.github.com>
2024-08-27 12:27:43 -0400

    Time: 4 ms (99.01%) | Memory: 42.4 MB (81.23%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,8 +1,7 @@
 class Solution {
-
-    private boolean checkEqual(int[] a, int[] b) {
-        for (int i = 0; i < 26; i++) {
-            if (a[i] != b[i]) {
+    private boolean checkEquals(int[] arr1, int[] arr2){
+        for(int i = 0; i < 26;i++){
+            if(arr1[i] != arr2[i]){
                 return false;
             }
         }
@@ -11,32 +10,28 @@ class Solution {
 
     public boolean checkInclusion(String s1, String s2) {
         int[] temp = new int[26];
-        for (int i = 0; i < s1.length(); i++) {
+        for(int i = 0; i < s1.length(); i++){
             temp[s1.charAt(i) - 'a']++;
         }
-
         int i = 0;
         int windowSize = s1.length();
-
         int[] temp1 = new int[26];
-        while (i < windowSize && i < s2.length()) {
+        while(i < windowSize && i < s2.length()) {
             temp1[s2.charAt(i) - 'a']++;
             i++;
         }
 
-        if (checkEqual(temp, temp1)) {
+        if(checkEquals(temp, temp1)){
             return true;
         }
-
-        while (i < s2.length()) {
+        while(i < s2.length()) {
             temp1[s2.charAt(i - windowSize) - 'a']--;
             temp1[s2.charAt(i) - 'a']++;
-            i++;
-            if (checkEqual(temp, temp1)) {
+            if(checkEquals(temp,temp1)){
                 return true;
             }
+            i++;
         }
-
         return false;
     }
 }
