18c8ddfe7edb064c331cad459482b1cdf0784135
Swapnil Mittal <72509297+swapnilmittal1@users.noreply.github.com>
2024-10-08 22:03:24 -0400

    Time: 31 ms (56.07%) | Memory: 51.1 MB (68.04%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,23 +1,18 @@
-
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        // Create a PriorityQueue that sorts based on distance in descending order
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> 
-            (b[0] * b[0] + b[1] * b[1]) - (a[0] * a[0] + a[1] * a[1])
-        );
+        int[][] ans = new int[k][2];
 
-        // Add points to the heap, maintaining only the k closest points
-        for (int[] point : points) {
-            maxHeap.offer(point);
-            if (maxHeap.size() > k) {
-                maxHeap.poll();  // Remove the farthest point if we have more than k
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>( (a,b) -> ( (b[0]*b[0] + b[1]*b[1]) - (a[0]*a[0] + a[1]*a[1]) ) );
+
+        for(int[] point : points){
+            minHeap.offer(point);
+            if (minHeap.size() > k){
+                minHeap.poll();
             }
         }
 
-        // Extract the k closest points from the heap
-        int[][] ans = new int[k][2];
-        for (int i = 0; i < k; i++) {
-            ans[i] = maxHeap.poll();
+        for(int i = 0; i < k; i++){    
+            ans[i] = minHeap.poll();
         }
 
         return ans;
