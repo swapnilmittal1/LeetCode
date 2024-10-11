0d54652e06d979143a21044f76f1e9f190495659
Swapnil Mittal <72509297+swapnilmittal1@users.noreply.github.com>
2024-10-11 15:56:36 -0400

    Time: 4 ms (99.20%) | Memory: 48.7 MB (16.70%) - LeetSync

--- 286-walls-and-gates/walls-and-gates.java
+++ 286-walls-and-gates/walls-and-gates.java
@@ -1,33 +1,25 @@
 class Solution {
-    public void wallsAndGates(int[][] rooms) {
-        Queue<int[]> q = new LinkedList<>();
-        int m = rooms.length;
-        int n = rooms[0].length;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (rooms[i][j] == 0) q.add(new int[] { i, j });
-            }
-        }
-        if (q.size() == 0) return;
-        int[][] dirs = { { -1, 0 }, { 0, -1 }, { 1, 0 }, { 0, 1 } };
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int row = cur[0];
-            int col = cur[1];
-            for (int[] dir : dirs) {
-                int x = row + dir[0];
-                int y = col + dir[1];
-                if (
-                    x >= m ||
-                    y >= n ||
-                    x < 0 ||
-                    y < 0 ||
-                    rooms[x][y] != Integer.MAX_VALUE
-                ) continue;
-                q.add(new int[] { x, y });
+    int height;
+    int width;
 
-                rooms[x][y] = rooms[row][col] + 1;
+    public void wallsAndGates(int[][] rooms) {
+        height = rooms.length;
+        width = rooms[0].length;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (rooms[i][j] == 0) dfs(rooms, i, j, 0);
             }
         }
+    }
+
+    void dfs(int[][] rooms, int m, int n, int dis) {
+        if (m < 0 || n < 0 || m >= height || n >= width || (dis != 0 && rooms[m][n] <= dis)) {
+            return;
+        } 
+        rooms[m][n] = dis;
+        dfs(rooms, m + 1, n, dis + 1);
+        dfs(rooms, m - 1, n, dis + 1);
+        dfs(rooms, m, n + 1, dis + 1);
+        dfs(rooms, m, n - 1, dis + 1);
     }
 }
