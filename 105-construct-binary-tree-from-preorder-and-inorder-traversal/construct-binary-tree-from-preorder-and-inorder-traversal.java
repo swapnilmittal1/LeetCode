4369e706405ba0b3dcf3c5126bf69c6e1e4f08a3
Swapnil Mittal <72509297+swapnilmittal1@users.noreply.github.com>
2024-10-14 16:11:41 -0400

    Time: 1 ms (96.84%) | Memory: 44.3 MB (44.30%) - LeetSync

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int preIndex = 0;
+    Map<Integer, Integer> map = new HashMap<>();
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for(int i = 0; i < inorder.length;i++){
+            map.put(inorder[i],i);
+        }
+        return helper(preorder, 0 , preorder.length - 1);
+    }
+
+    TreeNode helper(int[] preorder, int left, int right){
+        if (left > right) {
+            return null;
+        }
+        int index = preIndex;
+        preIndex++;
+
+        TreeNode root = new TreeNode(preorder[index]);
+        int mid = map.get(preorder[index]);
+
+        root.left = helper(preorder, left, mid - 1);
+        root.right = helper(preorder, mid + 1, right);
+
+        return root;
+
+
+
+    }
+}
