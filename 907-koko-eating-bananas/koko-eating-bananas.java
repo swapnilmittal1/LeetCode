c20467dd9e4ff55ca4f72d63c0f1ad98e284f4fc
Swapnil Mittal <72509297+swapnilmittal1@users.noreply.github.com>
2024-10-13 22:06:10 -0400

    Time: 13 ms (71.39%) | Memory: 45.1 MB (47.84%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -3,31 +3,37 @@ class Solution {
         int min = 1;
         int max = 0;
 
-        for(int n : piles){
-            if (max < n) {
-                max = n;
-            }
+        for (int n : piles){
+            max = Math.max(max, n);
         }
-        if (h == piles.length) {
+        if(h == piles.length){
             return max;
         }
 
-        while(min <= max){
-            int tempH = 0; int mid = (min + max) / 2;
-            for(int n : piles){
-                tempH += n/mid;
-                if(n % mid > 0){
-                    tempH++;
 
-                }}
-                if(tempH <= h && tempH > 0){
-                    max = mid - 1;
+        while(min <= max){
+            int time = 0;
+
+            int mid = (min + max) / 2;
+
+            for(int i = 0; i < piles.length;i++){
+
+                time += (piles[i])/mid;
+                if(piles[i] % mid > 0){
+                    time++;
                 }
-                else if(tempH > h || tempH < 0) {
+                }
+
+                if(time <= h && time > 0){
+                    max = mid - 1;
+                } else if(time > h || time < 0){
                     min = mid + 1;
                 }
+            }
+            
+            return min;
+
         }
-        return min;
+
 
     }
-}
