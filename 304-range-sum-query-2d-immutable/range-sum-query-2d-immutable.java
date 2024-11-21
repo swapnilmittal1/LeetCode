24ea6da2aa57a129a7591bf330cb25f0c586a503
Swapnil Mittal <72509297+swapnilmittal1@users.noreply.github.com>
2024-11-21 09:50:06 -0500

    Time: 100 ms (67.99%) | Memory: 65.8 MB (90.84%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -1,23 +1,35 @@
 class NumMatrix {
-    int[][] mat;
+    int[][] arr;
     public NumMatrix(int[][] matrix) {
-        int n=matrix.length,m=matrix[0].length;
-        for(int i=1;i<n;i++) matrix[i][0]+=matrix[i-1][0];
-        for(int i=1;i<m;i++) matrix[0][i]+=matrix[0][i-1];
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                matrix[i][j]+=(matrix[i-1][j]+matrix[i][j-1]-matrix[i-1][j-1]);
+        int l = matrix.length;
+        int w = matrix[0].length;
+        arr = matrix;
+
+        for(int i = 1; i < l; i++){
+            arr[i][0]  += arr[i-1][0];
+        }
+
+        for (int j = 1; j < w; j++) {
+            arr[0][j] += arr[0][j - 1];
+        }
+
+        for (int i = 1; i < l; i++) {
+            for (int j = 1; j < w; j++) {
+                arr[i][j] += arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1];
             }
         }
-        mat=matrix;
+
     }
     
-    public int sumRegion(int r1, int c1, int r2, int c2) {
-        int ans=mat[r2][c2];
-        if(r1>0) ans-=mat[r1-1][c2];
-        if(c1>0) ans-=mat[r2][c1-1];
-        if(r1>0 && c1>0) ans+=mat[r1-1][c1-1];
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int ans = arr[row2][col2];
+
+        if (row1 > 0) ans -= arr[row1 - 1][col2];
+        if (col1 > 0) ans -= arr[row2][col1 - 1];
+        if (row1 > 0 && col1 > 0) ans += arr[row1 - 1][col1 - 1];
+
         return ans;
+
     }
 }
 
