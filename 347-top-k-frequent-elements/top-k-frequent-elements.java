1def0584ad8c47fe185bceac11f12a445aa0e490
Swapnil Mittal <72509297+swapnilmittal1@users.noreply.github.com>
2025-02-13 20:03:30 -0500

    Time: 13 ms (75.15%) | Memory: 48.6 MB (67.62%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,25 +1,23 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer,Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        for(int i = 0; i < nums.length;i++){
+        for(int i = 0; i < nums.length;i++) {
             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
 
-        List<int[]> list = new ArrayList<>();
+        PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
 
-        for(Map.Entry<Integer,Integer> entry : map.entrySet()) {
-            list.add(new int[]{entry.getValue(),entry.getKey()});
-
+        for( Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            minHeap.offer(entry);
         }
-        list.sort((a,b) -> b[0] - a[0]);
-        int[] res = new int[k];
+        int[] ans = new int[k];
 
         for(int i = 0; i < k; i++){
-            res[i] = list.get(i)[1];
+            ans[i] = minHeap.poll().getKey();
+
         }
 
-        return res;
-
+        return ans;
     }
 }
